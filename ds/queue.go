5bdf4d46c884b10bcf1dbc16846e19f645639d26
Kavin Aravind <kvnaravind@gmail.com>
2024-05-16 11:27:37 -0400

    added doc comments to queue

--- ds/queue.go
+++ ds/queue.go
@@ -2,19 +2,23 @@ package ds
 
 import "sync"
 
+// Message is a generic message type
 type Message[T any] struct {
 	Data T
 }
 
+// Queue is a generic queue type
 type Queue[T any] struct {
 	messages []Message[T]
 	lock     sync.RWMutex
 }
 
+// NewQueue creates a new instance of the Queue
 func NewQueue[T any]() *Queue[T] {
 	return &Queue[T]{}
 }
 
+// Enqueue is used to add a message to the queue
 func (q *Queue[T]) Enqueue(message Message[T]) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
@@ -22,6 +26,7 @@ func (q *Queue[T]) Enqueue(message Message[T]) {
 	q.messages = append(q.messages, message)
 }
 
+// Dequeue is used to remove a message from the queue
 func (q *Queue[T]) Dequeue() (Message[T], bool) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
@@ -36,6 +41,7 @@ func (q *Queue[T]) Dequeue() (Message[T], bool) {
 	return message, true
 }
 
+// Copy is used to create a copy of the queue
 func (q *Queue[T]) Copy() *Queue[T] {
 	q.lock.RLock()
 	defer q.lock.RUnlock()
